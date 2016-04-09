edb1d8c6d536331b4c4427ea42a802d7d0673c54
Shayne Chow <shayne@weplus.cn>
2016-04-09 17:33:30 +0800

    Initial Commit

---
+++ WebSocketForiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebSocketForiOS
+//
+//  Created by Xiang on 16/4/9.
+//  Copyright © 2016年 周想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
