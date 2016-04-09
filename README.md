b4132f76bb0b6fc94d04cfdd3d7441808db14690
Shayne Chow <shayne@weplus.cn>
2016-04-09 18:49:19 +0800

    完成示例

---
+++ README.md
@@ -0,0 +1,7 @@
+# ZXFilter
+
+WebSocket for iOS
+
+===
+
+本示例程序使用了第三方库`SocketRocket`，实现了iOS客户端与服务器之间的WebSocket连接。
--- WebSocketForiOS/ViewController.m
+++ WebSocketForiOS/ViewController.m
@@ -7,21 +7,99 @@
 //
 
 #import "ViewController.h"
+#import <SocketRocket/SRWebSocket.h>
 
-@interface ViewController ()
+@interface ViewController ()<SRWebSocketDelegate, UITextFieldDelegate> {
+    SRWebSocket *_webSocket;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (weak, nonatomic) IBOutlet UITextField *messageTextField;
+@property (weak, nonatomic) IBOutlet UIButton *sendBtn;
+@property (weak, nonatomic) IBOutlet UITextView *showBox;
 
 @end
 
 @implementation ViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    _showBox.text = @"waiting for connection";
+    
+    [self reconnect:nil];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UITapGestureRecognizer *tgr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)];
+    [self.showBox addGestureRecognizer:tgr];
+    self.sendBtn.layer.cornerRadius = 5;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)hideKeyboard {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)reconnect:(id)sender {
+    _webSocket.delegate = nil;
+    [_webSocket close];
+    
+    _webSocket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"ws://10.10.9.203:8089"]]];
+    _webSocket.delegate = self;
+    
+    self.title = @"Opening Connection...";
+    [_webSocket open];
+    
+}
+
+- (IBAction)stopConnect:(id)sender {
+    _webSocket.delegate = nil;
+    [_webSocket close];
+    _webSocket = nil;
+    self.title = @"Connection Closed!";
+}
+
+- (IBAction)sendMessage:(id)sender {
+    [self hideKeyboard];
+    [_webSocket sendPing:nil];
+    [_webSocket send:_messageTextField.text];
+    _showBox.text = [NSString stringWithFormat:@"%@\n%@", _showBox.text, _messageTextField.text];
+    _messageTextField.text = nil;
+}
+
+#pragma mark - SRWebSocketDelegate
+
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket {
+    NSLog(@"Websocket Connected");
+    self.title = @"Connected!";
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    NSLog(@":( Websocket Failed With Error %@", error);
+    
+    self.title = @"Connection Failed! (see logs)";
+    _webSocket = nil;
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message {
+    NSLog(@"Received \"%@\"", message);
+    _showBox.text = [NSString stringWithFormat:@"%@\n%@", _showBox.text, message];
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
+    NSLog(@"WebSocket closed");
+    self.title = @"Connection Closed!";
+    _webSocket = nil;
+}
+
+- (void)webSocket:(SRWebSocket *)webSocket didReceivePong:(NSData *)pongPayload {
+    NSLog(@"Websocket received pong");
 }
 
 @end
